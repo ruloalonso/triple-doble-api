63f78c1b6c22a9f4478ef8142555ddd3e91b714a
ruloalonso <r.alonso.lopez@gmail.com>
2018-10-09 19:59:57 +0200

    feat: get past season stats

--- controllers/players.controller.js
+++ controllers/players.controller.js
@@ -23,18 +23,7 @@ module.exports.get = (req, res, next) => {
                 if (set[1] === '2017-18') {
                   console.log('stats found!!')
                   player.stats = {};
-                  player.stats.pastSeason = {
-                    gp: set[6],
-                    min: set[8],
-                    oreb: set[18],
-                    dreb: set[19],
-                    ast: set[21],
-                    stl: set[22],
-                    blk: set[23],
-                    tov: set[24],
-                    pf: set[25],
-                    pts: set[26]
-                  };
+                  player.stats.pastSeason = parseStats(set);
                 }
               })
               res.json(player);
@@ -50,40 +39,18 @@ module.exports.get = (req, res, next) => {
     });
 }
 
-module.exports.lastSeasonStats = (req, res, next) => {
-  Player.findById(req.params.id)
-    .then(player => {
-      if (!player) {
-        throw createError(404, 'Player not found');
-      } else {
-        PlayerStat.findOne({playerId: player.playerId})
-          .then((stat => {
-            if (!stat) {
-              throw createError(404, 'Stat not found');
-            } else {
-              let stats = {}
-              stat.data.rowSet.forEach((set) => {
-                if (set[1] === '2017-18') {
-                  stats = {
-                    gp: set[6],
-                    min: set[8],
-                    oreb: set[18],
-                    dreb: set[19],
-                    ast: set[21],
-                    stl: set[22],
-                    blk: set[23],
-                    tov: set[24],
-                    pf: set[25],
-                    pts: set[26]
-                  };
-                }
-              })
-              res.json(stats);
-            }
-          }))
-      }
-    })
-    .catch(error => {
-      next(error);
-    });
+function parseStats(set) {
+  let stats = {
+    gp: set[6],
+    min: set[8],
+    oreb: set[18],
+    dreb: set[19],
+    ast: set[21],
+    stl: set[22],
+    blk: set[23],
+    tov: set[24],
+    pf: set[25],
+    pts: set[26]
+  }
+  return stats;
 }
