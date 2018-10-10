9850c4054e2725c21c4b3e3e08eb71f0f7bcc9d0
Rulo Alonso <r.alonso.lopez@gmail.com>
2018-10-11 00:25:18 +0200

    add: league model

---
+++ models/league.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const leagueSchema = new mongoose.Schema({
+  admin: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  users: {
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: 'User'
+  },
+  status: {
+    type: String,
+    enum: ['application', 'draft', 'season'],
+    default: 'application'
+  },
+  round: {
+    type: Number,
+    default: 1
+  },
+  turn: {
+    type: Number,
+    default: 1
+  },
+  maxUsers: {
+    type: Number,
+    default: 3
+  },
+  maxRounds: {
+    type: Number,
+    default: 3
+  }
+});
+
+const League = mongoose.model('League', leagueSchema);
+module.exports = League;
